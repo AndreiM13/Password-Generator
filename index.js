a930e166e1fd0ecb804d3136003b3130e7871d21
Andrei Macovei <48647892+AndreiM13@users.noreply.github.com>
2025-12-10 11:04:37 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+let passwordFirst = document.getElementById("first-password")
+let passwordSecond = document.getElementById("second-password")
+
+
+function generatePassword() {
+    passwordFirst.textContent = ""
+    passwordSecond.textContent = ""
+
+    for (let i = 0; i < 15; i++) {  
+        let random1 = Math.floor(Math.random() * characters.length)
+        let random2 = Math.floor(Math.random() * characters.length)
+
+        passwordFirst.textContent += characters[random1]
+        passwordSecond.textContent += characters[random2]
+    }
+}
+
+
+generatePassword()
+
+
